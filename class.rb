fab787a5a97822174e83215540f8eed3b1f1741d
Mike <jarrellmike@gmail.com>
2013-06-19 21:37:04 +1000

    Edited the comments indicating group size muse be from 1-13

--- class.rb
+++ class.rb
@@ -4,21 +4,20 @@ group_size = gets.chomp.to_i
 
  while   group_size > 13 ||  group_size < 1
 
-            puts "Incorrect. What is your group size?"
+            puts "Group size must be between 1 and 13. Please enter your group size"
             group_size = gets.chomp.to_i
  end
 
 
 students = 'Mike Will Ryan Andrew Daniel Michael Tarun Hui Sanjita Dan Stephen Jack Nick'.split.shuffle
-
 number_of_groups = (students.size) / group_size.to_i
 
       number_of_groups.times do
-      puts "for this group"
+              puts "Names for this group"
 
       group_size.times do
-      puts students.pop
-    end
+              puts students.pop
+end
 end
 
 
