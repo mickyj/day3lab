fdf7ae2d03c022d1df7087c2b9f95d91dd7ea57f
wbvg-mac <will@mylittleshowpony.com>
2013-06-20 10:23:22 +1000

    Editing Comments

--- class.rb
+++ class.rb
@@ -1,43 +1,42 @@
 #Authors from GITHUB
 # WBVG
 # mickyj
-# OUR  CODE IS HERE => https://github.com/mickyj/day3lab
+# Our code is here => https://github.com/mickyj/day3lab
 # Please enjoy our random group distributor ~ From the boys!
 
+# Enter the # of Students: Our Array is split and shuffled all in one line of code.
+# If number of students = 13, edit the first group size arguement value, ie, 13 ||
+students = 'Mike Will Ryan Andrew Daniel Michael Tarun Hui Sanjita Dan Stephen Jack Nick'.split.shuffle
 
-
-#THIS IS THE MENU USER PROMPT
+# This is the menu user prompt
 puts "What is your group size?"
 group_size = gets.chomp.to_i
 
-#HERE WE DEFINE FALSE VALUES FOR ERROR PROMPTING TO USER AND RETURN TO MAIN MENU UNTIL CORRECT VALUES ARE PARSED
- while   group_size > 13 ||  group_size < 1
+# Here we define false values for error prompting to user and return to main menu until correct values are parsed
+while   group_size > 13 ||  group_size < 1
             puts "Group size must be between 1 and 13. Please enter your group size"
             group_size = gets.chomp.to_i
- end
+end
 
-# WE KNOW THAT THERE ARE 13 STUDENTS: Our Array is split and shuffled all in one line of code
-students = 'Mike Will Ryan Andrew Daniel Michael Tarun Hui Sanjita Dan Stephen Jack Nick'.split.shuffle
-
-#OUR EQUATIONS: WE USE .SIZE TO MEASURE OFF STUDENTS INTO EACH GROUP EVENLY
+# Our equations: we use .size to measure off students into each group evenly
 number_of_groups = (students.size) / group_size.to_i
 
-#THE NUMBER OF GROUPS LOOP - Here we evenly distribute students into groups defined by user input. We don't bother labelling our groups.
+# Here we evenly distribute students into groups defined by user input.
       number_of_groups.times do
               puts "Names for this group"
 
-# EACH TIME THE GROUP RUNS WE DELETE OFF THE EVEN DISTRIBUTION OF STUDENTS USING POP METHOD
+# Each time the group runs we delete off the even distribution of students using pop method
       group_size.times do
               puts students.pop
-end
+      end
 end
 
-# HERE WE NEED TO ADD THE 1 LONELY STUDENT TO THE LAST GROUP USING A PUSH
+# Add the 1 lonely student to the last group using a push
 if students != nil
   puts students.push
 end
 
-# AND THAT SHOULD BE IT!
+# voila!
 
 
 
